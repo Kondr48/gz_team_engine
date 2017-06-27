9b038ef4765cf76a34b7a4ac3a36a77fa0539ccc
Kondr48 <kondrashin.oleg35@yandex.ru>
2017-06-27 20:19:20 +0300

    Исправлен разлёт ворон из центра локации при загрузке игры
    Теперь вороны при загрузке локации будут летать сразу над актором.
    ---
    За этот и предыдущие коммиты спасибо Charsi

--- trunk/xray/xr_3da/xrGame/ai/crow/ai_crow.cpp
+++ trunk/xray/xr_3da/xrGame/ai/crow/ai_crow.cpp
@@ -123,6 +123,8 @@ void CAI_Crow::Load( LPCSTR section )
 
 }
 
+#include "../../Actor.h"
+
 BOOL CAI_Crow::net_Spawn		(CSE_Abstract* DC)
 {
 	BOOL R		= inherited::net_Spawn	(DC);
@@ -139,6 +141,12 @@ BOOL CAI_Crow::net_Spawn		(CSE_Abstract* DC)
 
 	// disable UpdateCL, enable only on HIT
 	processing_deactivate		();
+
+	Fvector tmp = Actor()->Position();
+	tmp.x = tmp.x + ::Random.randF	(-50.0f,50.0f);
+	tmp.y = tmp.y + ::Random.randF	(20.0f,50.0f);
+	tmp.z = tmp.z + ::Random.randF	(-50.0f,50.0f);
+	Position().set(tmp);
 
 	return		R;
 }
